b367b52838050164421ff0428a64b1455ecbaad0
LAShZ <lavch@qq.com>
2020-12-19 04:47:27 +0800

    modular functions

--- utils/calculate.js
+++ utils/calculate.js
@@ -15,6 +15,38 @@ function JudgeDivide(num1, num2){
   return (num1%num2)?false:true;
 }
 
+// calculate result
+function CalResult(num1,operator,num2){
+  switch (operator) {
+    case 0:
+      return num1+num2
+    case 1:
+      return num1-num2
+    case 2:
+      return num1*num2
+    case 3:
+      return num1/num2
+    default:
+      return 0
+  }
+}
+
+// convert operator into string
+function OptToString(opt){
+  switch (opt) {
+    case 0:
+      return '+'
+    case 1:
+      return '-'
+    case 2:
+      return 'X'
+    case 3:
+      return '÷'
+    default:
+      break;
+  }
+}
+
 //generate a question decided by the type
 function GenerateQuestion(type){
   switch (type) {
@@ -24,20 +56,10 @@ function GenerateQuestion(type){
       switch (operator) {
         case 0: 
           var num2 = RandomNum(0,10 - num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) + "+" + String(num2) + "=",
-            result:num1+num2
-          }
-          return question
+          break
         case 1:
           var num2 = RandomNum(0,num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) +"-" + String(num2) + "=",
-            result:num1-num2
-          }
-          return question
+          break
         default:
           break
       }
@@ -48,20 +70,10 @@ function GenerateQuestion(type){
       switch (operator) {
         case 0:
           var num2 = RandomNum(0,20 - num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) + "+" + String(num2) + "=",
-            result:num1+num2
-          }
-          return question
+          break
         case 1:
           var num2 = RandomNum(0,num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) +"-" + String(num2) + "=",
-            result:num1-num2
-          }
-          return question
+          break
         default:
           break
       }
@@ -72,20 +84,10 @@ function GenerateQuestion(type){
       switch (operator) {
         case 0:
           var num2 = RandomNum(0,100 - num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) + "+" + String(num2) + "=",
-            result:num1+num2
-          }
-          return question
+          break
         case 1:
           var num2 = RandomNum(0,num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) +"-" + String(num2) + "=",
-            result:num1-num2
-          }
-          return question
+          break
         default:
           break
       }
@@ -93,25 +95,17 @@ function GenerateQuestion(type){
     case 4:
       var num1 = RandomNum(1,9)
       var num2 = RandomNum(1,9)
-      var question = {
-        question_type:type,
-        expression:String(num1) +"X" + String(num2) + "=",
-        result:num1*num2
-      }
-      return question
+      var operator = 2
+      break
     case 5:
       var num1 = RandomNum(1,81)
       var num2 = RandomNum(1,9)
+      var operator = 3
       while((!JudgeDivide(num1,num2)) || ((num1/num2) >= 10)){
         num1 = RandomNum(1,81)
         num2 = RandomNum(1,9)
       }
-      var question = {
-        question_type:type,
-        expression:String(num1) +"÷" + String(num2) + "=",
-        result:num1/num2
-      }
-      return question
+      break
     // unsolved mixed computing
     case 6:
       var num1 = RandomNum(1,9)
@@ -119,8 +113,8 @@ function GenerateQuestion(type){
       var oprator2 = RandomNum(0,3)
       var question = {
         question_type:type,
-        expression:String(num1) +"÷" + String(num2) + "=",
-        result:num1/num2
+        expression:String(num1) + OptToString(3) + String(num2) + "=",
+        result:CalResult(num1,3,num2)
       }
       return question
     case 7:
@@ -130,20 +124,10 @@ function GenerateQuestion(type){
         case 0:
           var num2 = RandomNum(1,100 - num1)*(10**RandomNum(0,2))
           num1 = num1 *(10**RandomNum(0,2))
-          var question = {
-            question_type:type,
-            expression:String(num1) + "+" + String(num2) + "=",
-            result:num1+num2
-          }
-          return question
+         break
         case 1:
           var num2 = RandomNum(0,num1)
-          var question = {
-            question_type:type,
-            expression:String(num1) +"-" + String(num2) + "=",
-            result:num1-num2
-          }
-          return question
+          break
         default:
           break
       }
@@ -151,45 +135,36 @@ function GenerateQuestion(type){
     case 8:
       var num1 = RandomNum(0,200)
       var num2 = RandomNum(0,9)
-      var question = {
-        question_type:type,
-        expression:String(num1) +"X" + String(num2) + "=",
-        result:num1*num2
-      }
-      return question
+      break
     case 9:
-      var operator = RandomNum(0,1)
+      var operator = RandomNum(2,3)
       switch (operator) {
-        case 0:
+        case 2:
           var num1 = RandomNum(10,99)
           var num2 = RandomNum(1,9)*(10**RandomNum(0,1))
-          var question = {
-            question_type:type,
-            expression:String(num1) + "X" + String(num2) + "=",
-            result:num1*num2
-          }
-          return question
-        case 1:
+          break
+        case 3:
           var num1 = RandomNum(12,99)*(10**RandomNum(0,2))
           var num2 = RandomNum(1,9)
           while(!JudgeDivide(num1,num2)){
             num1 = RandomNum(12,99)*(10**RandomNum(0,2))
             num2 = RandomNum(1,9)
           }
-          var question = {
-            question_type:type,
-            expression:String(num1) +"÷" + String(num2) + "=",
-            result:num1/num2
-          }
-          return question
+          break
         default:
           break
       }
       break
     default:
-      break
+      window.alert("Error: Expression generate error!\n")
+      return
   }
-  window.alert("Error: Expression generate error!\n")
+  var question = {
+    question_type:type,
+    expression:String(num1) + OptToString(operator) + String(num2) + "=",
+    result:CalResult(num1,operator,num2)
+  }
+  return question
 }
 
 while(true){
